9f2cefa8751d54068eaca273f2c4832ca762f8e9
OlegAdmin <olegfil99@mail.ru>
2020-03-20 15:40:20 +0300

    Updated with DB access

--- src/ConnectinToDB.groovy
+++ src/ConnectinToDB.groovy
@@ -1,5 +1,8 @@
-class ConnectinToDB {
+@GrabConfig(systemClassLoader=true)
+@Grab(group='org.postgresql', module='postgresql', version='9.4-1205-jdbc42')
+import groovy.sql.Sql
 
-    
-
-}
+def sql = Sql.newInstance("jdbc:postgresql://localhost/labdb", "postgres", "mordheim", "org.postgresql.Driver")
+def results = sql.rows("select * from employees;")
+println results.size()
+sql.close()
